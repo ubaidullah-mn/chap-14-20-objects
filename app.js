847f8e9ae983aa0014f813b98f10430477ee3c0e
Ubaiduulah <ubaidullah5150@gmail.com>
2021-11-05 23:02:49 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,72 @@
+////Chap ARRAYS :
+///  ques 3:
+//String [] arr = new String [] {
+  //     "A", "B", "C", "D", "E"};
+//   ques 4 :
+//syntax:int[] myArray = {13 , 14, 15};
+//   ques 5 :
+//var anyBoxesChecked = []
+//var numeroPerguntas = 5 ;
+//for( i=0; i<numeroPerguntas; i++)
+//{
+ // anyBoxesChecked.push(false);
+//}
+//    ques 7:
+//var qualificationArr = [ "Qualifications:","SSC", "HSC","BCS", 
+ //      "BS", "BCOM", "MS", "M. Phil", "PhD"]
+//console.log("OLD Arr" , qualificationArr)
+//  ques :12
+//var arr = ["This ", "is", "my ", "cat"];
+//console.log( arr)
+// ques 10
+//var namesArr = ["karachi ", "lahore", "Islamabad", "Quetta", "Peshawar"];
+//console.log( "OLD Arr" , namesArr)
+//var copy = namesArr.slice(2 , 3)
+//var copy1 = namesArr.slice(4 , 5)
+//var copyName = [ namesArr.slice(2,4)[0] , copy1 ]
+//console.log("copy arr" , copyName)
+//var devicesArr = [ "Devices:","Keyboard", "Mouse","Printer","Pointer"]
+//console.log("OLD Arr" , devicesArr)
+////   Chap ARRAYS AND LOOPS
+//     ques 3:
+//for (var i = 1 ; i <=10 ; i++){
+ //   document.write(i + "<br>")
+//}
+//   ques 4:
+//var tableNum = +prompt("Enter a Number to show its multiplication table")
+//var tableLength = +prompt("Enter a table Length")
+//for (var i = 1 ; i<=tableLength ; i++){
+ //   document.write(tableNum + "x" + i + "=" + tableNum*1 + "<br>" )
+//}
+//   ques 5:
+//var fruitArr = ["apple","banana","mango","orange","strawberry"]
+//console.log(fruitArr, length)
+//   ques 6:
+//document.write("Countings:" + "<br>")
+//for (var i = 1 ; i <=15 ; i++){
+ //  document.write(i + "," )
+//}
+//document.write("Reverse Countings:" + "<br>")
+//for (var i = 10 ; i >=1 ; i--){
+// document.write(i + "," )
+//}
+//document.write("Even Number:" + "<br>")
+//for (var i = 0 ; i<=20 ; i = i+2){
+// document.write(i + "," )
+//}
+//document.write("Odd Number:" + "<br>")
+//for (var i = 1 ; i<=20 ; i = i+2){
+// document.write(i + "," )
+//}
+//document.write("Series:" + "<br>")
+//for (var i = 2 ; i<=20 ; i = i+2){
+ //document.write(i + "k" + "," )
+//}
+///    ques 10
+//document.write("Odd Number:" + "<br>")
+//for (var i = 5 ; i<=100 ; i = i+5){
+ //document.write(i + "," )
+//}
+
+
+
